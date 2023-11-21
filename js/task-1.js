6055aae53b91feae6b4b67b44c5ea87f144c4bc7
13Master13 <141366261+13Master13@users.noreply.github.com>
2023-11-21 21:21:38 +0200

    Setings

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-function isEnoughCapacity(products, containerSize) {
-	let totalProducts = 0
-	for (const count of Object.values(products)) {
-		totalProducts += count
-	}
-	return totalProducts <= containerSize
-}
